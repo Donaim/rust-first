668f797b2d37c4ac1374af14ff328e84be4985d2
d0naim <d0naim@jerrs.dsnet.umcs.pl>
2018-02-02 00:40:21 +0100

    created window!!!

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,11 +1,15 @@
 // extern crate conrod;
 
+extern crate rand;
+extern crate clap;
+
 extern crate conrod;
+extern crate find_folder;
 
 mod window;
 
 fn main() {
     println!("HELLO");
 
-    window::ww::create();
+    window::main();
 }
--- hello_world/src/window.rs
+++ hello_world/src/window.rs
@@ -1,7 +1,82 @@
 use conrod;
+use find_folder;
 
-pub mod ww {
-    pub fn create() {
-        println!("HELLO FROM WINDOW MOTHERFUCKER!");
-    }
+// #[derive(PartialEq, Eq)]
+// pub enum State {
+// 	MainMenu,
+// 	Solo(char,[[char;3];3]),
+// 	Multiplayer(char,[[char;3];3]),
+// 	End(char),
+// }
+
+// widget_ids! {
+// 	pub struct Ids {
+//         canvas,
+// 		solo,
+// 		multiplayer,
+// 		text,
+// 		replay,
+// 		quit,
+// 		case[],
+// 	}
+// }
+
+pub fn main() {
+	use conrod::backend::piston::{self, Window, WindowEvents, OpenGL};
+	use conrod::backend::piston::event::UpdateEvent;
+
+	let mut window: Window = piston::window::WindowSettings::new("Tic Tac Toe", [512, 512])
+        	.opengl(OpenGL::V3_2)
+        	.build()
+        	.unwrap();
+
+	let mut events = WindowEvents::new();
+
+	let mut text_texture_cache = piston::window::GlyphCache::new(&mut window, 512, 512);
+
+	let mut ui = conrod::UiBuilder::new([512.0 as f64, 512.0 as f64]).build();
+	// let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+	// let font_path = assets.join("fonts/NotoSans/NotoSans-Bold.ttf");
+	// ui.fonts.insert_from_file(font_path).unwrap();
+	// let mut ids = Ids::new(ui.widget_id_generator());
+	// ids.case.resize(10, &mut ui.widget_id_generator());
+
+	// let mut state = &mut State::MainMenu;
+	// let mut node = ai::Node::new();
+	// let mut played_node = Vec::new();
+
+	while let Some(event) = window.next_event(&mut events) {
+	// 	if let Some(e) = piston::window::convert_event(event.clone(), &window) {
+	// 		if let conrod::event::Input::Release(btn) = e {
+	// 			if let conrod::input::Button::Keyboard(key) = btn {
+	// 				if key == conrod::input::Key::Escape {
+	// 					*state = State::MainMenu;
+	// 				}
+	// 			}
+	// 		}
+	// 		ui.handle_event(e);
+    //     	}
+
+	// 	event.update(|_| {
+	// 		match *state {
+	// 			State::MainMenu					=> main_menu::	update(&window, &ids, &mut ui, &mut state, &mut node, &mut played_node),
+	// 			State::Multiplayer(player, terrain)		=> multiplayer::update(&window, &ids, &mut ui, &mut state, player, terrain),
+	// 			State::Solo(_,_)				=> solo::	update(&window, &ids, &mut ui, &mut state, &mut node, &mut played_node),
+	// 			State::End(winner)				=> end::	update(&window, &ids, &mut ui, &mut state, winner),
+	// 		}
+	// 	});
+
+	// 	window.draw_2d(&event, |c, g| {
+	// 		if let Some(primitives) = ui.draw_if_changed() {
+	// 			fn texture_from_image<T>(img: &T) -> &T {img};
+    //             		piston::window::draw(
+	// 				c,
+	// 				g,
+	// 				primitives,
+    //                     		&mut text_texture_cache,
+    //                             	&image_map,
+    //                     		texture_from_image);
+	// 		}
+	// 	});
+	}
 }
